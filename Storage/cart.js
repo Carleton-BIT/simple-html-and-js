efea567fafe5ca0900f751fd3a4fcab74d9709ef
James Brunet <james@brunet.dev>
2024-12-02 12:45:34 -0500

    Finish classroom example

--- Storage/cart.js
+++ Storage/cart.js
@@ -1,13 +1,30 @@
 function loadCart() {
     console.log(`Loaded page for the first time!`)
+    let cartItems = document.querySelector('#cart-items')
+    cartItems.innerHTML = ''
+
+    let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+    cart.forEach((item) => {
+        const li = document.createElement('li')
+        li.textContent = item
+        cartItems.appendChild(li)
+    })
+    
+
 }
 
 function addToCart(item) {
-    console.log(`Added ${item} to cart!`)
+    let cart = JSON.parse(localStorage.getItem("cart")) || []
+    cart.push(item)
+    localStorage.setItem("cart", JSON.stringify(cart))
+    loadCart()
 }
 
 function clearCart() {
     console.log(`Cleared cart!`)
+    localStorage.removeItem('cart')
+    loadCart()
 }
 
 document.addEventListener('DOMContentLoaded', loadCart);
